98e65d9f49525199e36a2e0ca039faac469e7081
Yee-Siang <ccs1419@gmail.com>
2022-01-16 01:32:55 +0800

    deploy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: new URL('/api', window.location.origin).toString(),
+  baseURL: new URL('/', window.location.origin).toString(),
 });
 
 export default instance;
