b5aae2f72a0cc7fea40659d8b7314a5eccfe8f58
chienjerry <55382066+chienjerry@users.noreply.github.com>
2022-01-16 12:49:41 +0800

    Add files via upload

--- model/user.js
+++ model/user.js
@@ -2,14 +2,11 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     UserID :    String,
-    Password :  String,
+    Password :  String, //Password的用處僅為驗證碼，而非密碼
     Nickname :  String,
     School :    String,
     AboutMe :   String,
     Birthday : [String ,String ,String ]
-
-    /*UserKey: { type: String, unique: true },*/
-    /*TimeStamp: Date*/
 },{
     collection: 'User'
 
