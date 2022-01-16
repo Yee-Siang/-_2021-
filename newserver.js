12db657f447a0907110a938d8a270f21db68c97a
YellowJason <jason90814@gmail.com>
2022-01-16 17:04:48 +0800

    test test

--- newserver.js
+++ newserver.js
@@ -49,7 +49,7 @@ db.once("open", () => {
 const __dirname = dirname(fileURLToPath(import.meta.url));
 console.log(__dirname);
 app.use(express.static(path.join(__dirname, "build")));
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 ///////////////////////////////////////////////////////////////////////////////
