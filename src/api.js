20cceebd28aeed75b1aad2e36a91f132fcb1e62f
Yee-Siang <ccs1419@gmail.com>
2022-01-16 15:49:27 +0800

    new

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: new URL('/', window.location.origin).toString(),
+  //baseURL: new URL('/', window.location.origin).toString(),
+  baseURL: 'http://mm2021final.herokuapp.com',
 });
 
 export default instance;
